7f9a3ce74a2fa54bc26db838f6a844c993517684
Kim Anh Nguyen <kimanhou@gmail.com>
2021-12-05 18:32:37 +0100

    Text about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,10 +5,28 @@ const About : React.FC = props => {
     return (
         <div className={`about`}>
             <h1>À propos des RT</h1>
-            <p>Les Royal Tannins, c’est quoi ? <br></br>
-                Les RT, ce sont les copains. <br></br>
-                Les copains qui aiment partager, rire, vivre ensemble. <br></br>
-                Les RT, c’est boire du vin, bien manger, se faire plaisir.
+            <div className={`spacer`}></div>
+            <p>Au départ, Royal Tannins n’étaient qu’une blague entre copains !</p>
+            <p>
+                Un jour de décembre, passé la trentaine, quelques gais lurons férus de vins décident de participer à une formation oenologique (WSET1). Une fois celle-ci en poche, ils décident de poursuivre l’aventure en se réunissant pour découvrir des vins d’ici et d’ailleurs, sans prétention, toujours guidés par la simple soif de découverte !
+            </p>
+            <p>
+                Au fil du temps, l’un d’entre eux les surnomment les Royal Tannins, puis l’idée germe dans leur tête : et si on pouvait réunir des amateurs de vins, de tous horizons, pour réaliser des activités autour de cette passion ?
+                <br></br>
+                C’est alors que l’aventure a débuté et que les Royal Tannins sont nés.
+            </p>
+            <p>
+                Les Royal Tannins ont pour but de découvrir des pépites viticoles par le biais de dégustations, axées sur des thèmes ciblés, dans une multitude de lieux, ainsi que de rencontrer tout.e.s ces femmes et hommes qui participent à la confection de breuvage (cavistes, vignerons, artisans, etc.).
+            </p>
+            <p>
+                Amateurs de vins blancs, rouges, rosés, jaunes, de paille, champagnes, venez découvrir le monde du vin avec simplicité et convivialité, en ayant l’envie de partager des bons moments autour de vrais produits à boire et à manger.
+            </p>
+            <p>
+                N’hésitez pas à nous contacter pour en savoir plus.
+            </p>
+            <div className={`spacer`}></div>
+            <p className={`right`}>
+                - Les Royal Tannins.
             </p>
         </div>
     );
